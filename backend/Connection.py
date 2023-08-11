56fd4a8e9f508fd9895339fe08b70269b477a28c
Tomaslopera <loperatomas410@gmail.com>
2023-08-11 16:49:37 -0500

    Primera Entrega Backend

---
+++ backend/Connection.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+class Connection:
+    
+    def database_connection():
+        try:
+            conexion = mysql.connector.connect(user='root', password='root_password',
+                                   host='localhost', database='recircular', # localhost = 127.0.0.1
+                                   port='3306')
+            return conexion
+            
+        except mysql.connector.Error as error:
+            print('Error al conectarte a la base de datos {}'.format(error))
