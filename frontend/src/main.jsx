ac53ec50d90cd322ad3480a968423ef76580f243
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-08-27 13:20:28 -0500

    loadingContext agregado

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,19 +4,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserContextProvider } from './context/userContext';
+import { LoadingContextProvider } from './context/loadingContext';
 import Sesion from './components/Sesion';
 import App from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route path='/sesion' element={<Sesion />} />
-        </Routes>
-      </BrowserRouter>
-    </UserContextProvider>
+
+    {/* Contextos */}
+    <LoadingContextProvider>
+      <UserContextProvider>
+
+        {/* Ruteo */}
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='/sesion' element={<Sesion />} />
+          </Routes>
+        </BrowserRouter>
+
+      </UserContextProvider>
+    </LoadingContextProvider>
+
   </React.StrictMode>
 );
