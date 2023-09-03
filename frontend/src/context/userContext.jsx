466b19227f4e84d07a70f24342b6264305454848
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-09-03 17:33:13 -0500

    funciones del contexto

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useContext, useState } from "react"
+import axios from "axios"
+import { validarEmail, validarPassword } from "../functions/Formularios";
 
 
 const userContext = createContext();
@@ -13,20 +15,71 @@ export const useUser = () => {
 export const UserContextProvider = (props) => {
     const [user, setUser] = useState(null);
 
-    const getUser = async (usuario) => {
-        
-    }
+    const createUser = async ({ username, email, password }) => {
+        try {
+            // Validacion de parametros
+            const existe = await getUser(username);
+            if (existe) return "Este usuario ya existe";
 
-    const createUser = async (username, email, password) => {
-        console.log(username, email, password)
-    }
+            if (!validarEmail(email)) return "Email no valido";
+            if (!validarPassword(password)) return "La contraseña no es valida";
 
+            // Crear nuevo usuario
+            const usuario = {
+                username,
+                email,
+                password
+            };
+            
+            await axios.post("/usuarios", usuario);
+            
+            setUser(usuario);
+            return true;
+        } catch (error) {
+            return "Intentelo más tarde";         
+        }
+    }
+    
+    const deleteUser = async (buscar) => {
+        try {
+            // Buscar usuario en bd
+            const usuario = await getUser(buscar);
+            if (!usuario) return "El usuario no existe";
+    
+            // Inactivar usuario
+            usuario.activo = false;
+            return editUser(usuario);
+        } catch (error) {
+            return false;
+        }
+    }
+    
     const editUser = async (usuario) => {
         
     }
 
-    const deleteUser = async (usuario) => {
-        
+    const getUser = async (buscar) => {
+        try {
+            // Se puede obtener por id o por username
+            const response = await axios.get("/usuarios/"+buscar);
+            return response;
+        } catch (error) {
+            return false;
+        }
+    }
+    
+    const login = async ({ username, password }) => {
+        // Obtener usuario
+        const usuario = await getUser(username);
+
+        // Validar password
+        const iguales = usuario.getPassword===password;
+
+        // En caso de no coincidir devolver mensaje
+        if (!usuario || !iguales) return "Username o password no validos";
+
+        setUser(usuario);
+        return true;
     }
 
     return (
@@ -34,9 +87,11 @@ export const UserContextProvider = (props) => {
             value={{
                 user,
                 createUser,
-                editUser,
                 deleteUser,
-                getUser
+                editUser,
+                getUser,
+                login,
+                setUser
             }}
         >
             {props.children}
