c5483c62b74ea6364e5677653db7f5b20881c547
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-09-03 11:56:44 -0500

    Layout creado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import Header from './components/Header';
-
+import Layout from "./components/Layout"
 
 const App = () => {
-
   return (
-    <Header />
+    <Layout>
+
+    </Layout>
   )
 }
 
-export default App
+export default App;
