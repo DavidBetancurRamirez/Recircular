3e4595fa66cbb02958b53fdcc1a42d1980dcb2f4
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-09-27 15:57:22 -0500

    Material agregado

---
+++ frontend/src/elements/Material.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+import colores from "../styles/colores"
+
+const ContenedorMaterial = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 8px;
+    width: 100px;
+    height: 30px;
+    border-radius: 20px;
+    background-color: ${colores.verdeClaro};
+    margin-right: 5px;
+    text-transform: capitalize;
+    font-size: 14px;
+
+    span {
+        width: 12px;
+        height: 12px;
+        border: 1px solid ${colores.azulOscuro};
+        border-radius: 50%;
+    }
+`
+
+const Material = ({ material }) => {
+    return (
+        <ContenedorMaterial>
+            <p>{material.nombre}</p>
+            <span style={{ backgroundColor: material.color }} />
+        </ContenedorMaterial>
+    )
+}
+
+export default Material;
